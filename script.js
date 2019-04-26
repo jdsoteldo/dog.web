6e33337d9f65112fcb54f6bf4fc2e4cd0b4fbf22
Juan Diego <jdsoteldo00@gmail.com>
2019-04-25 19:46:40 -0500

    dog web

---
+++ script.js
@@ -0,0 +1,9 @@
+//alert('Hello!');
+
+var first_name= prompt("What's your name?");
+
+var last_name= prompt("What's your last name?")
+
+var age= prompt("How old r u?")
+
+ console.log (` Your name is ${first_name} ${last_name} and you r ${age} old`)
